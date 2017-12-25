fabd79d9ac46d42487f456dd9d59da4659a9d18b
YuliyaHryshchankova <yuliya_hryshchankova@epam.com>
2017-12-25 23:23:00 +0300

    Task3 with quantity calculation (partially)

--- src/com/company/lecturetwo/homework/test/NominatorTest.java
+++ src/com/company/lecturetwo/homework/test/NominatorTest.java
@@ -4,6 +4,8 @@ import com.company.lecturetwo.homework.vo.AwardVO;
 import com.company.lecturetwo.homework.vo.NominatorVO;
 import com.company.lecturetwo.homework.vo.NomineeVO;
 
+import java.util.Random;
+
 public class NominatorTest {
     public static void main(String[] args) {
         NominatorVO nominator = new NominatorVO("Tom");
@@ -11,13 +13,52 @@ public class NominatorTest {
         NomineeVO nominee2 = new NomineeVO("Alex");
         NomineeVO nominee3 = new NomineeVO("Kris");
 
+        // ------ Dave awards
+
         nominator.nominate(nominee1, new AwardVO(45, 0.75));
         nominator.nominate(nominee1, new AwardVO(70, 1.03));
-        nominator.nominate(nominee2, new AwardVO(150));
+
+        // ------ Alex awards
+
+        AwardVO alexAward1 = new AwardVO(150);
+        AwardVO alexAward2 = new AwardVO(100);
+        AwardVO alexAward3 = new AwardVO(75);
+
+        nominator.nominate(nominee2, alexAward1);
+        nominator.nominate(nominee2, alexAward2);
+        nominator.nominate(nominee2, alexAward3);
+
+        double alexQuantity1 = calculateQuantity(alexAward1.getValue(), 3);
+        double alexQuantity2 = calculateQuantity(alexAward2.getValue(), 3);
+        double alexQuantity3 = calculateQuantity(alexAward3.getValue(), 3);
+
         nominator.nominate(nominee2, new AwardVO(320, 1.40));
+
+        // ----- Kris awards ---------
         nominator.nominate(nominee3, new AwardVO(100, 0.23));
         nominator.nominate(nominee3, new AwardVO(25, 1.5));
-    }
 
+        System.out.println("Quantity of Alex's award1 is " + alexQuantity1);
+        System.out.println("Quantity of Alex's award2 is " + alexQuantity2);
+        System.out.println("Quantity of Alex's award3 is " + alexQuantity3);
+
+        System.out.println("Quantity of Alex's award1 is " + (alexQuantity1 > alexQuantity2 ? "greater than" : (alexQuantity1 == alexQuantity2) ? "equal to" : "less than") + " quantity of Alex's award2.");
+        //System.out.println("");
+
+    }
+    //статический метод раcчета quantity
+    static double calculateQuantity(int value, int population) {
+        //объявление переменных для расчета quantity
+        Random random = new Random();
+        double c = random.nextDouble();
+        int z = random.nextInt();
+        //int population = 3;
+        //все статические методы можно вызывать из класса, не создавая объекта, т.к. они принадлежат всему классу
+        //Math.pow(z, 2) - возведение в квадрат объекта z
+        double a = (Math.pow(z, 2)*value*(1-value))/Math.pow(c, 2);
+        double quantity = a/(1+((a-1)/population));
+        return quantity;
+
+    }
 
 }
--- src/com/company/lecturetwo/homework/vo/NomineeVO.java
+++ src/com/company/lecturetwo/homework/vo/NomineeVO.java
@@ -1,5 +1,7 @@
 package com.company.lecturetwo.homework.vo;
 
+import java.util.Random;
+
 public class NomineeVO {
 
     //статический блок инициализации
@@ -7,10 +9,11 @@ public class NomineeVO {
         System.out.println("Class NomineeVO initialized");
     }
 
-    //приватное строкое поле name
+    //приватное строковое поле name. Все переменные в классе обычно private, если не надо другое.
     private String name;
+    private double quantity;
 
-    //конструктор
+    //конструктор(используется для создания объектов данного класса NomineeVO)
     public NomineeVO(String name) {
         this.name = name;
     }
@@ -19,16 +22,18 @@ public class NomineeVO {
     public void receiveAward(AwardVO award) {
         double soli = award.getSoli();
         int value = award.getValue();
-        //
+        //если задано soli, то пересчитываем value и выводим запись об этом
         if (soli > 0 && soli != 1) {
             double newValue = value * soli;
             System.out.println("Recalculated award value is " + newValue + ".\n" + "Soli value is " + soli + ".");
-
+            //оббъявление переменной difference
             double difference;
+            //если soli < 1, то считаем потерю и выводим в консоль
             if (soli < 1) {
                 difference = (1 - soli) * 100;
                 System.out.println("Award value reduced by " + difference + "%");
             } else {
+                //если soli > 1, то считаем, на сколько увеличилась value и выводим разницу в консоль
                 difference = (soli - 1) * 100;
                 System.out.println("Award value increased by " + difference + "%");
 
@@ -40,6 +45,10 @@ public class NomineeVO {
         }
 
         System.out.println("-----------");
+    }
+
+    public double getQuantity() {
+        return quantity;
     }
 
     public String getName() {
